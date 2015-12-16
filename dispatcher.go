e46230d04100f525dd4f048f694c89d9ab7093a1
Sumit Asok <sumitasok@gmail.com>
2015-12-16 23:12:05 +0530

    get body data

--- dispatcher.go
+++ dispatcher.go
@@ -1,8 +1,10 @@
 package apis
 
 import (
+	"io/ioutil"
 	"net/http"
 
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
 )
@@ -13,26 +15,49 @@ type D struct {
 
 	actions []action
 
-	mgoClone *mgo.Session
-	rw       *http.ResponseWriter
-	req      *http.Request
+	req http.Request
 }
 
-func (d *D) GetMgoSession() *mgo.Session {
-	if d.c.mgo == nil {
-		d.LogInfo("Mongo DB not initiated")
-		return nil
-	}
+type DB *mgo.Session
 
-	if d.mgoClone != nil {
-		return d.mgoClone
-	}
+// func (d *D) GetMgoSession() *mgo.Session {
+// 	if d.c.mgo == nil {
+// 		d.LogInfo("Mongo DB not initiated")
+// 		return nil
+// 	}
 
-	d.mgoClone = d.c.mgo.Clone()
-	return d.mgoClone
-}
+// 	if d.mgoClone != nil {
+// 		return d.mgoClone
+// 	}
+
+// 	d.mgoClone = d.c.mgo.Clone()
+// 	return d.mgoClone
+// }
 
 func (d D) Call(rw http.ResponseWriter, req *http.Request, params httprouter.Params) {
+}
+
+func (d *D) Body(i interface{}) error {
+	body, err := d.reqToByteArray()
+	if err != nil {
+		d.LogInfo("Cannot convert Request body to byte array", err.Error())
+	}
+
+	err = json.Unmarshal(body, &i)
+	if err != nil {
+		d.LogInfo("data couldn't be marshalled", err.Error())
+	}
+
+	return err
+}
+
+func (d *D) reqToByteArray() ([]byte, error) {
+	body, err := ioutil.ReadAll(d.req.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
 }
 
 func (d *D) LogTrace(trace ...interface{}) {
--- dispatcher_test.go
+++ dispatcher_test.go
@@ -12,3 +12,46 @@ func TestDispatcherLoggerShouldBeInitiated(t *testing.T) {
 
 	assert.True(true)
 }
+
+type data struct {
+	value int
+}
+type pointer struct {
+	data *data
+	nonP int
+}
+
+func (p pointer) call() int {
+	return p.data.value
+}
+
+func (p pointer) alt() {
+	p.nonP = 2
+	p.data.value = 3
+}
+
+func TestPointerData(t *testing.T) {
+	assert := assert.New(t)
+
+	p := pointer{data: &data{value: 1}}
+	q := &p
+	r := p
+
+	assert.Equal(1, p.call())
+	assert.Equal(0, p.nonP)
+
+	p.data.value = 2
+
+	assert.Equal(2, p.call())
+	assert.Equal(0, p.nonP)
+
+	assert.Equal(2, q.call())
+
+	p.alt()
+	assert.Equal(3, p.call())
+	assert.Equal(0, p.nonP)
+	assert.Equal(3, q.call())
+	assert.Equal(3, r.call())
+
+	assert.True(true)
+}
