9ea4fd4d545f25bbf719483da955d4bbad065129
Sumit Asok <sumit.asok@bookmyshow.com>
2016-06-13 16:05:39 +0530

    config comments

--- c.go
+++ c.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-// Init
+// Init initiates a context for the web application
 // Init() -> returns the context on which the whole app is going to be built on
 func Init() *C {
 	c := &C{&context{}}
@@ -25,14 +25,17 @@ type C struct {
 	*context
 }
 
+// LogRequest set true to enable logging, if in production you want to disable logging for performance, use this methodd to set it false
 func (c *C) LogRequest(status bool) {
 	c.context.logRequest = status
 }
 
+// NewDispatcher returns a new dispatcher with context set
 func (c *C) NewDispatcher() *D {
 	return &D{c: c.context}
 }
 
+// SetMgo sets the mongodb when address is passed. Tries to connect and exits if no connection was found.
 func (c *C) SetMgo(addr string) *C {
 	err := c.setMgo(addr)
 
@@ -51,6 +54,7 @@ func (c *C) setLogger() *C {
 	return c
 }
 
+// Listen call listen with port number to start your server at this port number.
 func (c *C) Listen(port int) {
 	// Routes
 	c.attachRoutes()
@@ -65,18 +69,22 @@ func (c *C) Listen(port int) {
 	http.ListenAndServe(":"+strconv.Itoa(port), c.router)
 }
 
+// Get basic Get route
 func (c *C) Get(url string) *route {
 	return &route{context: c, method: "GET", url: url}
 }
 
+// Post basic post route
 func (c *C) Post(url string) *route {
 	return &route{context: c, method: "POST", url: url}
 }
 
+// Put basic Put route
 func (c *C) Put(url string) *route {
 	return &route{context: c, method: "PUT", url: url}
 }
 
+// Del basic del route
 func (c *C) Del(url string) *route {
 	return &route{context: c, method: "DELETE", url: url}
 }
--- config.go
+++ config.go
@@ -9,13 +9,16 @@ import (
 )
 
 const (
+	// ConfigPathSeperator '.' if config path is 'data.db.addr' is dot seperated
 	ConfigPathSeperator = "."
 )
 
+// Config returns the config of the application
 func (c *C) Config() *Config {
 	return c.context.appConfig
 }
 
+// ConfigHttpRouter gets the config from an http server which returns a json formated config data.
 func (c *C) ConfigHttpResp(resp *http.Response, err error) *C {
 	if err != nil {
 		c.infoLog.Fatal(err)
@@ -36,6 +39,7 @@ func (c *C) ConfigHttpResp(resp *http.Response, err error) *C {
 	return c
 }
 
+// ConfigFile reads the config from a json file
 func (c *C) ConfigFile(filepath string) *C {
 
 	data, err := ioutil.ReadFile(filepath)
@@ -48,6 +52,7 @@ func (c *C) ConfigFile(filepath string) *C {
 	return c
 }
 
+// ConfigByte returns the config from Int data
 func (c *C) ConfigBytes(data []byte) *C {
 	j, err := simplejson.NewJson(data)
 	if err != nil {
@@ -59,6 +64,7 @@ func (c *C) ConfigBytes(data []byte) *C {
 	return c
 }
 
+// Root brings the node and sets the root node to the level specified by parameters eg: ("data", "db")
 func (c *C) Root(nodes ...string) *C {
 	for i := range nodes {
 		if j, ok := c.appConfig.data.CheckGet(nodes[i]); ok {
@@ -71,15 +77,19 @@ func (c *C) Root(nodes ...string) *C {
 	return c
 }
 
+// Config holds the details about the config
 type Config struct {
 	allowFallback bool
 	data          *simplejson.Json
 }
 
+// AllowConfig is set to true takes the fallback value passed as second paramter if the input config doesnot have the value
+// if this is set, you might forget adding config to your config file or server.
 func (c *Config) AllowFallback(status bool) {
 	c.allowFallback = status
 }
 
+// Bytes returns the value if it is an array
 func (c *Config) Array(path string, fallback []interface{}) []interface{} {
 	node, ok := lastConfigNode(path, *c.data)
 	if !ok {
@@ -100,6 +110,7 @@ func (c *Config) Array(path string, fallback []interface{}) []interface{} {
 	return val
 }
 
+// Bool returns the value if it is a Bool
 func (c *Config) Bool(path string, fallback bool) bool {
 	node, ok := lastConfigNode(path, *c.data)
 	if !ok {
@@ -120,6 +131,7 @@ func (c *Config) Bool(path string, fallback bool) bool {
 	return val
 }
 
+// Bytes returns the value if it is a Bytes
 func (c *Config) Bytes(path string, fallback []byte) []byte {
 	node, ok := lastConfigNode(path, *c.data)
 	if !ok {
@@ -140,6 +152,7 @@ func (c *Config) Bytes(path string, fallback []byte) []byte {
 	return val
 }
 
+// Float returns the value if it is a Float
 func (c *Config) Float(path string, fallback float64) float64 {
 	node, ok := lastConfigNode(path, *c.data)
 	if !ok {
@@ -160,6 +173,7 @@ func (c *Config) Float(path string, fallback float64) float64 {
 	return val
 }
 
+// String returns the value if it is a String
 func (c *Config) String(path string, fallback string) string {
 	node, ok := lastConfigNode(path, *c.data)
 	if !ok {
@@ -204,6 +218,7 @@ func nextConfigNode(keys []string, node *simplejson.Json) (*simplejson.Json, boo
 	}
 }
 
+// Int returns the value if it is a Int
 func (c *Config) Int(path string, fallback int) int {
 	node, ok := lastConfigNode(path, *c.data)
 	if !ok {
@@ -224,6 +239,7 @@ func (c *Config) Int(path string, fallback int) int {
 	return val
 }
 
+// Int64 returns the value if it is a Int64
 func (c *Config) Int64(path string, fallback int64) int64 {
 	node, ok := lastConfigNode(path, *c.data)
 	if !ok {
@@ -244,6 +260,7 @@ func (c *Config) Int64(path string, fallback int64) int64 {
 	return val
 }
 
+// Interface returns the value if it is a Interface
 func (c *Config) Interface(path string, fallback interface{}) interface{} {
 	node, ok := lastConfigNode(path, *c.data)
 	if !ok {
@@ -257,6 +274,7 @@ func (c *Config) Interface(path string, fallback interface{}) interface{} {
 	return val
 }
 
+// Map returns the value if it is a Map
 func (c *Config) Map(path string, fallback map[string]interface{}) map[string]interface{} {
 	node, ok := lastConfigNode(path, *c.data)
 	if !ok {
@@ -277,6 +295,7 @@ func (c *Config) Map(path string, fallback map[string]interface{}) map[string]in
 	return val
 }
 
+// StringArray returns the value if it is a StringArray
 func (c *Config) StringArray(path string, fallback []string) []string {
 	node, ok := lastConfigNode(path, *c.data)
 	if !ok {
@@ -297,6 +316,7 @@ func (c *Config) StringArray(path string, fallback []string) []string {
 	return val
 }
 
+// Uint64 returns the value if it is a Uint64
 func (c *Config) Uint64(path string, fallback uint64) uint64 {
 	node, ok := lastConfigNode(path, *c.data)
 	if !ok {
